89bc011de3cad62edd3159e63dab596fc12cbe51
Renirahmawati24 <renirahmawati019>
2023-11-08 10:24:02 +0700

    tugastabel

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,34 @@
+<hl>Tugas</hl>
+Reni Rahmawati
+
+<IDOCTYPE html>
+<html>
+<style>
+table, th, td {
+	border:1px solid black;
+}
+</style>
+<body>
+
+<h2>table latihan</h2>
+
+<table style-"width:100">
+<tr>
+	<th>Nama</th>
+	<th>Alamat</th>
+	<th>No Telp</th>
+</tr>
+<tr>
+	<td>Ani Suryani</td>
+	<td>Jln Pesantren</td>
+	<td>08987777777</td>
+</tr>
+<tr>
+	<td>Khalifa</td>
+	<td>Sariwangi</td>
+	<td>08563773888</td>
+</tr> 
+</table>
+
+</body>
+</html>
